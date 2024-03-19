1c90cb9d66e264455ec8e08a7cf6c873448fc60a
JaviMx <javimxmine@hotmail.com>
2024-03-19 09:52:25 -0600

    Add PUT endpoint for updating todos
    Implemented a PUT endpoint  for updating existing todo items. Ensured the endpoint properly handles validations and updates the todo item in the database. Integrated logic to verify if the todo item exists before attempting an update. Added error handling for scenarios where a todo item cannot be found or updated.

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,7 @@
 import prisma from "@/lib/prisma";
+import { Todo } from "@prisma/client";
 import { NextResponse, NextRequest } from "next/server";
+import { boolean, object, string } from "yup";
 
 interface Segments {
   params: {
@@ -7,14 +9,40 @@ interface Segments {
   };
 }
 
+const getTodo = async (id: string): Promise<Todo | null> => {
+  return await prisma.todo.findFirst({ where: { id } });
+};
+
 export async function GET(request: Request, { params }: Segments) {
-  const todo = await prisma.todo.findFirst({
-    where: {
-      id: params.id,
-    },
-  });
+  const todo = await getTodo(params.id);
+  if (!todo)
+    return NextResponse.json({ message: "Id not found" }, { status: 404 });
+
+  return NextResponse.json(todo);
+}
+
+const putSchema = object({
+  complete: boolean().optional(),
+  description: string().optional(),
+});
+
+export async function PUT(request: Request, { params }: Segments) {
+  const todo = await getTodo(params.id);
   if (!todo)
     return NextResponse.json({ message: "El id not found" }, { status: 404 });
 
-  return NextResponse.json(todo);
+  try {
+    const { complete, description } = await putSchema.validate(
+      await request.json()
+    );
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id: params.id },
+      data: { complete, description },
+    });
+
+    return NextResponse.json(updatedTodo);
+  } catch (error) {
+    return NextResponse.json(error, { status: 400 });
+  }
 }
